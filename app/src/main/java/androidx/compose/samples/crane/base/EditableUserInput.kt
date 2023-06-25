75d1ce48dddf2dd78f6f7a48e071934c79cbd511
pavlo-buslovskyi <pavlo.buslovskyi@globallogic.com>
2023-06-25 17:45:54 +0300

    State holder callers

--- app/src/main/java/androidx/compose/samples/crane/base/EditableUserInput.kt
+++ app/src/main/java/androidx/compose/samples/crane/base/EditableUserInput.kt
@@ -38,6 +38,10 @@ class EditableUserInputState(private val hint: String, initialText: String) {
     val isHint: Boolean
         get() = text == hint
 
+    fun updateText(newText: String) {
+        text = newText
+    }
+
     companion object {
         val Saver: Saver<EditableUserInputState, *> = listSaver(
             save = { listOf(it.hint, it.text) },
@@ -59,28 +63,21 @@ fun rememberEditableUserInputState(hint: String): EditableUserInputState =
 
 @Composable
 fun CraneEditableUserInput(
-    hint: String,
+    state: EditableUserInputState = rememberEditableUserInputState(""),
     caption: String? = null,
     @DrawableRes vectorImageId: Int? = null,
-    onInputChanged: (String) -> Unit
 ) {
     // TODO Codelab: Encapsulate this state in a state holder
-    var textState by remember { mutableStateOf(hint) }
-    val isHint = { textState == hint }
-
     CraneBaseUserInput(
         caption = caption,
-        tintIcon = { !isHint() },
-        showCaption = { !isHint() },
+        tintIcon = { !state.isHint },
+        showCaption = { !state.isHint },
         vectorImageId = vectorImageId
     ) {
         BasicTextField(
-            value = textState,
-            onValueChange = {
-                textState = it
-                if (!isHint()) onInputChanged(textState)
-            },
-            textStyle = if (isHint()) {
+            value = state.text,
+            onValueChange = { state.updateText(it) },
+            textStyle = if (state.isHint) {
                 captionTextStyle.copy(color = LocalContentColor.current)
             } else {
                 MaterialTheme.typography.body1.copy(color = LocalContentColor.current)
